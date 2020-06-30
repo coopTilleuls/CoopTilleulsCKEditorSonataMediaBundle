2774d2cf5e23e85a9f19d0c5f42269e7c3acc8ba
Maxime AURIAU <maxime.auriau@protonmail.com>
2020-06-30 16:23:52 +0200

    fix deprencated tree builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -28,8 +28,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('coop_tilleuls_ck_editor_sonata_media');
+        $treeBuilder = new TreeBuilder('coop_tilleuls_ck_editor_sonata_media');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('coop_tilleuls_ck_editor_sonata_media');
+        }
 
         $rootNode
             ->children()
