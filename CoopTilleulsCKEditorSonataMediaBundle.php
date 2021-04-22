f5935153fa76eaa90b4cdd9da1ca1710c074cef4
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2021-04-22 17:38:11 +0100

    remove the usage of `Bundle::getParent()` (#39)

--- CoopTilleulsCKEditorSonataMediaBundle.php
+++ CoopTilleulsCKEditorSonataMediaBundle.php
@@ -18,13 +18,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class CoopTilleulsCKEditorSonataMediaBundle extends Bundle
 {
-    /**
-     * Allows to extends the MediaAdmin controller.
-     *
-     * {@inheritdoc}
-     */
-    public function getParent()
-    {
-        return 'SonataMediaBundle';
-    }
 }
